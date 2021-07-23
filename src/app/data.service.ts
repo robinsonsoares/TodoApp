deab334a48541c5bd53a15f39bd24275d74d3175
Robinson Soares <soares.robinson@gmail.com>
2021-07-23 09:46:55 -0300

    Altering Components

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -18,4 +18,25 @@ export class DataService {
       return null;
     }
   }
+
+  public getTodayTodos(token) {
+    return this.http.get(`${this.baseUrl}/v1/todos/undone/today`, { headers: this.composeHeaders(token) });
+  }
+
+  public getTomorrowTodos(token) {
+    return this.http.get(`${this.baseUrl}/v1/todos/undone/tomorrow`, { headers: this.composeHeaders(token) });
+  }
+
+  public getAllTodos(token) {
+    return this.http.get(`${this.baseUrl}/v1/todos`, { headers: this.composeHeaders(token) });
+  }
+
+  public postTodo(data, token) {
+    return this.http.post(`${this.baseUrl}/v1/todos`, data, { headers: this.composeHeaders(token) });
+  }
+
+  public markAsDone(data, token) {
+    return this.http.put(`${this.baseUrl}/v1/todos/mark-as-done`, data, { headers: this.composeHeaders(token) });
+  }
+
 }
--- src/app/pages/new/new.component.ts
+++ src/app/pages/new/new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-new',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private service: DataService,
+    private router: Router,
+    private afAuth: AngularFireAuth,
+  ) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])],
+      date: [new Date().toJSON().substring(0, 10), Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    this.afAuth.idToken.subscribe(token => {
+      this.service.postTodo(this.form.value, token)
+        .subscribe(res => {
+          this.router.navigateByUrl("/");
+        });
+    });
   }
 
 }
